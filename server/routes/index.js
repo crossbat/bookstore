9128063b9dd35e6c428d3afabb4891bc6bc502d3
crossbat <xowls1999@gmail.com>
2025-03-28 12:24:55 +0900

    3월 27일-리액트 라우팅 및 서버 데이터 전달

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.json({
+    title: '메인화면입니다.',
+    description: '메인화면에 대한 설명입니다.'
+  });
 });
 
 module.exports = router;
